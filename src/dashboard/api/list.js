3d3da7855e976a4a9dd9c9dfe5eb4df78949ddec
FAREAST\jili10 <jili10@microsoft.com>
2023-07-06 17:16:13 +0800

    update

--- src/dashboard/api/list.js
+++ src/dashboard/api/list.js
@@ -16,10 +16,10 @@ export async function getDataFromDB() {
         }
       }`;
 
-  const endpoint = "/data-api/graphql/Person";
+  const endpoint = "/data-api/graphql";
   const response = await fetch(endpoint, {
       method: "POST",
-      headers: { "Content-Type": "application/json","X-MS-API-ROLE": "admin" },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ query: query })
   });
   const result = await response.json();
