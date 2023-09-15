358ecb5d7191e8ce059ad796c27bb3f9e7553cf2
unknown <danluo@nvidia.com>
2023-09-15 13:51:09 +0800

    duapet

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ export default function App () {
   const [startDate, setStartDate] = React.useState(() => {
     const d = new Date();
     // Set it to 7 days ago ago
-    d.setDate(d.getDate() - 10);
+    d.setDate(d.getDate() - 2);
     return dayjs(d);
   });
 
